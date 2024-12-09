8087bc6f6f420fd064eecab5ae6f8a5761653865
EduBarb0sa <edus.barbosa16@gmail.com>
2024-12-09 14:58:56 -0300

    Adiconando algumas linhas esquecidas

--- Questão 8.hs
+++ Questão 8.hs
@@ -1,3 +1,13 @@
+{- QUESTÃO 8  VALOR: 1,0 ponto
+
+ Defina a função "geraReceituarioPlano", cujo tipo é dado abaixo e que retorna um receituário válido a partir de um
+ plano de medicamentos válido.
+ Dica: Existe alguma relação de simetria entre o receituário e o plano de medicamentos? Caso exista, essa simetria permite
+ compararmos a função geraReceituarioPlano com a função geraPlanoReceituario ? Em outras palavras, podemos definir
+ geraReceituarioPlano com base em geraPlanoReceituario ?
+
+-}
+
 -- Função para adicionar um horário à lista de horários de um medicamento no receituário
 adicionarHorario :: Medicamento -> Horario -> Receituario -> Receituario
 adicionarHorario med hor [] = [(med, [hor])]
@@ -26,3 +36,5 @@ convertePlanoParaReceituario ((hor, meds):resto) =
 -- Função principal para gerar o receituário a partir do plano de medicamentos
 geraReceituarioPlano :: PlanoMedicamento -> Receituario
 geraReceituarioPlano plano = convertePlanoParaReceituario plano
+
+-- Check 100%
--- Questão 9.hs
+++ Questão 9.hs
@@ -1,3 +1,12 @@
+{-  QUESTÃO 9 VALOR: 1,0 ponto
+
+Defina a função "executaPlantao", cujo tipo é dado abaixo e que executa um plantão válido a partir de um estoque de medicamentos,
+resultando em novo estoque. A execução consiste em desempenhar, sequencialmente, todos os cuidados para cada horário do plantão.
+Caso o estoque acabe antes de terminar a execução do plantão, o resultado da função deve ser Nothing. Caso contrário, o resultado
+deve ser Just v, onde v é o valor final do estoque de medicamentos
+
+-}
+
 atualizaEstoque :: Cuidado -> EstoqueMedicamentos -> Maybe EstoqueMedicamentos
 atualizaEstoque (Comprar med qtd) estoque = Just (adicionarMedicamento med qtd estoque)
 atualizaEstoque (Medicar med) estoque = consumirMedicamento med estoque
@@ -24,3 +33,5 @@ executaPlantao ((_, cuidados):resto) estoque = executaCuidados cuidados estoque 
 executaCuidados :: [Cuidado] -> EstoqueMedicamentos -> Maybe EstoqueMedicamentos
 executaCuidados [] estoque = Just estoque
 executaCuidados (c:cs) estoque = atualizaEstoque c estoque >>= executaCuidados cs
+
+-- Chech 100%
